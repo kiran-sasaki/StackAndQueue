27a31a803ff73b4a2a88b5c811f0650cc23ada8e
kiran-sasaki <kiransekar1111@gmail.com>
2022-08-17 12:36:47 +0530

    Day14

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StackQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.Insert(70);
+            //linkedListStack.Insert(30);
+            //linkedListStack.Insert(56);
+            //linkedListStack.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //linkedListStack.Display();
+
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+            linkedListQueue.Dequeue();
+            linkedListQueue.Display();
+        }
+    }
+}
