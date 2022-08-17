be5fd1a9c6d10fa8324197b5fc6fcfc308feb3a6
kiran-sasaki <kiransekar1111@gmail.com>
2022-08-17 12:39:39 +0530

    day14

---
+++ LinkedListQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    public class LinkedListQueue
+    {
+        Node head = null;
+
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head=node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted to queue", node.data);
+
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public void Dequeue()
+        {
+            if(this.head == null)
+            {
+                Console.WriteLine("queue is empty");
+                return;
+            }
+            Console.WriteLine("value dequeued is {0}",this.head.data );
+            this.head = this.head.next;
+        }
+    }
+}
